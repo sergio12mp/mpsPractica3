4a279c7228174067f39f54819dcbd2c2f79e74b4
sergioM <sermorepe@gmail.com>
2022-03-31 13:04:43 +0200

    all test done + class modified

--- src/main/java/ad/AdvertisementBoard.java
+++ src/main/java/ad/AdvertisementBoard.java
@@ -6,75 +6,88 @@ import java.util.List;
 /**
  * Board to publish advertisements
  *
- @author Antonio J. Nebro
+ * @author Antonio J. Nebro
  */
 public class AdvertisementBoard {
-  private static int MAX_BOARD_SIZE = 20 ;
-  private List<Advertisement> advertisementList;
+    private static int MAX_BOARD_SIZE = 3;
+    private List<Advertisement> advertisementList;
 
-  /**
-   * Constructor
-   */
-  public AdvertisementBoard() {
-    advertisementList = new ArrayList<>();
-    Advertisement advertisement = new Advertisement(
-        "Welcome",
-        "This board is intended for your advertisements",
-        "THE Company");
-    advertisementList.add(advertisement);
-  }
-
-  /**
-   * Returns the number of advertisements published
-   * @return
-   */
-  public int numberOfPublishedAdvertisements() {
-    return advertisementList.size();
-  }
-
-  /**
-   * Publishes an advertisement. The advertiser called "THE Company" is the owner of the advertisement
-   * board, so it can publish freely, without any constraints
-   *
-   */
-  public void publish(Advertisement advertisement,
-      AdvertiserDatabase advertiserDatabase,
-      PaymentDatabase paymentDatabase) {
-    if (advertisement.advertiser.equals("THE Company"))
-      advertisementList.add(advertisement);
-    else {
-      if ((advertiserDatabase.findAdviser(advertisement.advertiser)) &&
-              (paymentDatabase.advertiserHasFunds(advertisement.advertiser))) {
+    /**
+     * Constructor
+     */
+    public AdvertisementBoard() {
+        advertisementList = new ArrayList<>();
+        Advertisement advertisement = new Advertisement(
+                "Welcome",
+                "This board is intended for your advertisements",
+                "THE Company");
         advertisementList.add(advertisement);
-        paymentDatabase.advertisementPublished(advertisement.advertiser);
-      }
     }
-  }
 
-  /**
-   * Finds an advertisement form its title
-   *
-   * @param
-   * @return
-   */
-  public Advertisement findByTitle(String title) {
-    Advertisement result = null;
-    for (Advertisement advertisement : advertisementList) {
-      if (advertisement.title.equals(title))
-         result = advertisement;
+    /**
+     * Returns the number of advertisements published
+     *
+     * @return
+     */
+    public int numberOfPublishedAdvertisements() {
+        return advertisementList.size();
     }
-    return result;
-  }
 
-  /**
-   * Deletes an advertisement from its name and advertiser
-   * @param title
-   * @param advertiserName
-   */
-  public void deleteAdvertisement(String title, String advertiserName) {
-    for (Advertisement advertisement : advertisementList) {
-      if ((advertisement.title.equals(title) && (advertisement.advertiser.equals(advertiserName))))
-        advertisementList.remove(advertisement);
+    /**
+     * Publishes an advertisement. The advertiser called "THE Company" is the owner of the advertisement
+     * board, so it can publish freely, without any constraints
+     */
+    public void publish(Advertisement advertisement,
+                        AdvertiserDatabase advertiserDatabase,
+                        PaymentDatabase paymentDatabase) {
+
+        if(advertisementList.size()==MAX_BOARD_SIZE){
+            throw new RuntimeException();
+        }
+        if (advertisement.advertiser.equals("THE Company") && (advertisementList.size() < MAX_BOARD_SIZE))
+            advertisementList.add(advertisement);
+        else {
+            for (Advertisement ad : advertisementList) {
+                if ((ad.title.equals(advertisement.title) && (ad.advertiser.equals(advertisement.advertiser)))) {
+
+                    return;
+                }
+            }
+            if ((advertiserDatabase.findAdviser(advertisement.advertiser)) &&
+                    (paymentDatabase.advertiserHasFunds(advertisement.advertiser)) && (advertisementList.size() < MAX_BOARD_SIZE)) {
+                advertisementList.add(advertisement);
+                paymentDatabase.advertisementPublished(advertisement.advertiser);
+
+            }
+        }
     }
-  }
+
+    /**
+     * Finds an advertisement form its title
+     *
+     * @param
+     * @return
+     */
+    public Advertisement findByTitle(String title) {
+        Advertisement result = null;
+        for (Advertisement advertisement : advertisementList) {
+            if (advertisement.title.equals(title))
+                result = advertisement;
+        }
+        return result;
+    }
+
+    /**
+     * Deletes an advertisement from its name and advertiser
+     *
+     * @param title
+     * @param advertiserName
+     */
+    public void deleteAdvertisement(String title, String advertiserName) {
+        for (Advertisement advertisement : advertisementList) {
+            if ((advertisement.title.equals(title) && (advertisement.advertiser.equals(advertiserName)))) {
+                advertisementList.remove(advertisement);
+            }
+        }
+    }
 }
--- src/test/java/ad/AdvertisementBoardTest.java
+++ src/test/java/ad/AdvertisementBoardTest.java
@@ -3,35 +3,137 @@ package ad;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.test.context.TestExecutionListeners;
 
 class AdvertisementBoardTest {
     @Test
     public void ABoardHasAnAdvertisementWhenItIsCreated() {
-
+        var ad = new Advertisement("titulo", "texto", "anunciador");
+        var tablon = new AdvertisementBoard();
+        assertNotEquals(0, tablon.numberOfPublishedAdvertisements());
     }
 
     @Test
     public void PublishAnAdvertisementByTheCompanyIncreasesTheNumberOfAdvertisementsByOne() {
+        var ad = new Advertisement("titulo", "texto", "THE Company");
+        var tablon = new AdvertisementBoard();
+        int previusSize = tablon.numberOfPublishedAdvertisements();
+
+        PaymentDatabase PagoBD = Mockito.mock(PaymentDatabase.class);
+        AdvertiserDatabase AnuncianteBD = Mockito.mock(AdvertiserDatabase.class);
+
+        tablon.publish(ad, AnuncianteBD, PagoBD);
+
+        assertEquals(previusSize + 1, tablon.numberOfPublishedAdvertisements());
+
     }
 
     @Test
     public void WhenAnAdvertiserHasNoFoundsTheAdvertisementIsNotPublished() {
+        var ad = new Advertisement("titulo", "texto", "Pepe Gotera y Otilio");
+        var tablon = new AdvertisementBoard();
+        int previusSize = tablon.numberOfPublishedAdvertisements();
+
+        PaymentDatabase PagoBD = Mockito.mock(PaymentDatabase.class);
+        AdvertiserDatabase AnuncianteBD = Mockito.mock(AdvertiserDatabase.class);
+
+        Mockito.when(AnuncianteBD.findAdviser("Pepe Gotera y Otilio")).thenReturn(false);
+
+        tablon.publish(ad, AnuncianteBD, PagoBD);
+
+        assertEquals(previusSize, tablon.numberOfPublishedAdvertisements());
+
     }
 
     @Test
     public void AnAdvertisementIsPublishedIfTheAdvertiserIsRegisteredAndHasFunds() {
+
+        var ad = new Advertisement("titulo", "texto", "Robin Robot");
+        var tablon = new AdvertisementBoard();
+        int previousSize = tablon.numberOfPublishedAdvertisements();
+
+        PaymentDatabase PagoBD = Mockito.mock(PaymentDatabase.class);
+        AdvertiserDatabase AnuncianteBD = Mockito.mock(AdvertiserDatabase.class);
+
+        Mockito.when(AnuncianteBD.findAdviser("Robin Robot")).thenReturn(true);
+        Mockito.when(PagoBD.advertiserHasFunds("Robin Robot")).thenReturn(true);
+        tablon.publish(ad, AnuncianteBD, PagoBD);
+
+        assertEquals(previousSize + 1, tablon.numberOfPublishedAdvertisements());
+
     }
 
     @Test
     public void WhenTheOwnerMakesTwoPublicationsAndTheFirstOneIsDeletedItIsNotInTheBoard() {
+
+        var ad1 = new Advertisement("titulo1", "texto1", "THE Company");
+        var ad2 = new Advertisement("titulo2", "texto2", "THE Company");
+
+        var tablon = new AdvertisementBoard();
+
+
+        PaymentDatabase PagoBD = Mockito.mock(PaymentDatabase.class);
+        AdvertiserDatabase AnuncianteBD = Mockito.mock(AdvertiserDatabase.class);
+
+        tablon.publish(ad1, AnuncianteBD, PagoBD);
+        tablon.publish(ad2, AnuncianteBD, PagoBD);
+
+        int previousSize = tablon.numberOfPublishedAdvertisements();
+
+        tablon.deleteAdvertisement("titulo1", "THE Company");
+
+        assertNull(tablon.findByTitle("titulo1"));
+        assertNotEquals(previousSize - 1, tablon.numberOfPublishedAdvertisements());
+
     }
 
     @Test
     public void AnExistingAdvertisementIsNotPublished() {
+        var ad1 = new Advertisement("titulo", "texto", "anunciador");
+        var ad2 = new Advertisement("titulo", "texto", "anunciador");
+        var tablon = new AdvertisementBoard();
+
+
+        PaymentDatabase PagoBD = Mockito.mock(PaymentDatabase.class);
+        AdvertiserDatabase AnuncianteBD = Mockito.mock(AdvertiserDatabase.class);
+
+        Mockito.when(AnuncianteBD.findAdviser("anunciador")).thenReturn(true);
+        Mockito.when(PagoBD.advertiserHasFunds("anunciador")).thenReturn(true);
+
+        tablon.publish(ad1, AnuncianteBD, PagoBD);
+        tablon.publish(ad2, AnuncianteBD, PagoBD);
+
+        int previousSize = tablon.numberOfPublishedAdvertisements();
+
+
+        assertEquals(previousSize, tablon.numberOfPublishedAdvertisements());
+        Mockito.verify(AnuncianteBD, Mockito.times(1)).findAdviser("anunciador");
+
     }
 
     @Test
     public void AnExceptionIsRaisedIfTheBoardIsFullAndANewAdvertisementIsPublished() {
+
+
+        var ad1 = new Advertisement("titulo1", "texto1", "THE Company");
+        var ad2 = new Advertisement("titulo2", "texto2", "THE Company");
+        var ad3 = new Advertisement("titulo3", "texto3", "Tim O'Theo");
+
+        var tablon = new AdvertisementBoard();
+
+        PaymentDatabase PagoBD = Mockito.mock(PaymentDatabase.class);
+        AdvertiserDatabase AnuncianteBD = Mockito.mock(AdvertiserDatabase.class);
+
+        Mockito.when(AnuncianteBD.findAdviser("Tim O'Theo")).thenReturn(true);
+        Mockito.when(PagoBD.advertiserHasFunds("Tim O'Theo")).thenReturn(true);
+
+        tablon.publish(ad1, AnuncianteBD, PagoBD);
+        tablon.publish(ad2, AnuncianteBD, PagoBD);
+        int previousSize = tablon.numberOfPublishedAdvertisements();
+
+        assertThrows(RuntimeException.class, () -> tablon.publish(ad3, AnuncianteBD, PagoBD));
+        assertEquals(previousSize, tablon.numberOfPublishedAdvertisements());
+
     }
 }
